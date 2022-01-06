29f1261dc5429df49d114216f5e4660c37703372
Behruz <bboisxonov@yandex.com>
2022-01-07 02:52:20 +0500

    tezlik va masofadan vaqtni topish amallar

---
+++ js/main.js
@@ -0,0 +1,61 @@
+var masofaElement = document.getElementById("input")
+var tezlikElement = document.getElementById("tezlik")
+var btnElement = document.getElementById("btn")
+var resultPiyoda = document.getElementById("resultPiyoda")
+var resultVelosiped = document.getElementById("resultVelosiped")
+var resultMashina = document.getElementById("resultMashina")
+var resultSamolyot = document.getElementById("resultSamolyot")
+var formElement = document.getElementById("form")
+
+function hisobla(masofa,tezlik){
+    var result = masofa/tezlik
+    return `${Math.floor(result)} soat ${Math.floor((result-Math.floor(result))*60)}minut`
+}
+
+// function hisobla(masofa,tezlik){
+//     var result = masofa/tezlik
+//     return `${Math.floor(result)} soat ${Math.floor(result-Math.floor(result))*60}minut ${Math.floor(Math.floor(result))*60}sicund`
+// }
+
+var piyoda = 3.6
+var velosiped = 20.1
+var mashina = 70
+var samolyot = 800
+
+formElement.addEventListener('submit',(event) => {
+    event.preventDefault()
+    console.log("input jonatildi")
+
+    if(masofaElement.value == "" || masofaElement.value == 0){
+        resultPiyoda.textContent = "masofa hato kiritildi"
+        resultPiyoda.style.color = "red"
+        
+        resultVelosiped.textContent = "masofa hato kiritildi"
+        resultVelosiped.style.color = "red"
+
+        resultMashina.textContent = "masofa hato kiritildi"
+        resultMashina.style.color = "red"
+
+        resultSamolyot.textContent = "masofa hato kiritildi"
+        resultSamolyot.style.color = "red"
+    }else{
+        resultPiyoda.textContent = hisobla(Number(masofaElement.value),piyoda)
+        resultPiyoda.style.color = "green"
+
+        resultVelosiped.textContent = hisobla(Number(masofaElement.value),velosiped)
+        resultVelosiped.style.color = "green"
+
+        resultMashina.textContent = hisobla(Number(masofaElement.value),mashina)
+        resultMashina.style.color = "green"
+
+        resultSamolyot.textContent = hisobla(Number(masofaElement.value),samolyot)
+        resultSamolyot.style.color = "green"
+
+    }
+})
+
+
+// function hisobla(masofa,tezlik){
+//     var result = masofa/tezlik
+//     return `${Math.floor(result)} soat ${Math.floor(result-Math.floor(result))*60}minut ${Math.floor(Math.floor(result))*60}sicund`
+// }
